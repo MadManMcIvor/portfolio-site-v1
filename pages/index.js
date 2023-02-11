b0aa2767699f648e9537bde68ff5e9e2878704c1
Alex McIvor <alexmcivor@Alexs-MBP.attlocal.net>
2023-02-11 12:04:54 -0800

    gonna start moving stuff around

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import {AiFillLinkedin, AiFillGithub, AiFillGitlab} from 'react-icons/ai'
 import {useState} from 'react'
 
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png"
+import alex from "../public/alex_avatar.png"
 import design from '../public/design.png'
 import code from '../public/code.png'
 import consulting from '../public/consulting.png'
@@ -28,12 +28,12 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className=' bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-700'>
+      <main className=' bg-slate-50 px-10 md:px-20 lg:px-40 dark:bg-slate-600'>
         <section className="min-h-screen">
           <nav className='py-10 mb-12 flex justify-between'>
-            <h1 className='text-xl font-burtons'>developed by</h1>
+            <h1 className='text-xl font-burtons dark:text-white'>developed by</h1>
             <ul className='flex items-center'>
-              <li><BsFillMoonStarsFill className='cursor-pointer' onClick={() => setDarkMode(!darkMode)}/></li>
+              <li><BsFillMoonStarsFill className='cursor-pointer dark:text-white' onClick={() => setDarkMode(!darkMode)}/></li>
               <li>
                 <a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href='#'>Resume</a>
               </li>
@@ -41,26 +41,26 @@ export default function Home() {
           </nav>
           <div className=' text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium md:text-6xl '>Alex McIvor</h2>
-            <h3 className='text-2xl py-2 md:text-3xl'>Full Stack Software Engineer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto'>I'm a driven full stack software engineer. HIRE ME FOR THE LOVE OF GOD!</p>
+            <h3 className='text-2xl py-2 md:text-3xl dark:text-white'>Full Stack Software Engineer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-xl mx-auto dark:text-white'>I'm a driven full stack software engineer. HIRE ME FOR THE LOVE OF GOD!</p>
           </div>
-          <div className=' text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+          <div className=' text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-white'>
             <AiFillLinkedin />
             <AiFillGithub />
             <AiFillGitlab />
           </div>
           <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 overflow-hidden md:h-96 md:w-96'>
-            <Image src={deved} layout="fill" objectFit="cover" />
+            <Image src={alex} layout="fill" objectFit="cover" />
           </div>
         </section>
         
         <section>
           <div>
-            <h3 className=' text-3xl py-1'>Services I offer</h3>
-            <p className='text-md py-5 leading-8 text-gray-800'>
+            <h3 className=' text-3xl py-1 dark:text-white'>Services I offer</h3>
+            <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'>
               Blah blah blah Then I did a thing blah. <span className=' text-teal-500'>This is where I can highlight things</span> blah asdfasdf asdf asdf fs fs adfasdf asdf af adfafa afasfdfa f afsf af afaf.
             </p>
-            <p className='text-md py-5 leading-8 text-gray-800'>
+            <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'>
               Blah blah blah Then I did a thing blah. blah asdfasdf asdf asdf fs fs adfasdf asdf af adfafa afasfdfa f afsf af afaf.
             </p>
           </div>
@@ -105,11 +105,11 @@ export default function Home() {
 
           <section>
             <div>
-              <h3 className=' text-3xl py-1'>Portfolio</h3>
-              <p className='text-md py-5 leading-8 text-gray-800'>
+              <h3 className=' text-3xl py-1 dark:text-white'>Portfolio</h3>
+              <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'>
                 Blah blah blah Then I did a thing blah. <span className=' text-teal-500'>This is where I can highlight things</span> blah asdfasdf asdf asdf fs fs adfasdf asdf af adfafa afasfdfa f afsf af afaf.
               </p>
-              <p className='text-md py-5 leading-8 text-gray-800'>
+              <p className='text-md py-5 leading-8 text-gray-800 dark:text-white'>
                 Blah blah blah Then I did a thing blah. blah asdfasdf asdf asdf fs fs adfasdf asdf af adfafa afasfdfa f afsf af afaf.
               </p>
             </div>
